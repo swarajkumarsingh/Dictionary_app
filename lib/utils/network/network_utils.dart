1c6dd6f42c23d4fbf71202ed0ddeeea57f776a4d
swarajkumarsingh <sswaraj169@gmail.com>
2023-05-04 20:36:14 +0530

    fixed networkUtils

--- lib/utils/network/network_utils.dart
+++ lib/utils/network/network_utils.dart
@@ -1,25 +1,30 @@
+import 'package:dictionary/utils/sf.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 import '../../common/check_internet_screen.dart';
 import '../../home/screens/home_screen.dart';
+import '../../onboard/screens/onboarding_screen.dart';
 import '../navigator.dart';
 import '../snackbar.dart';
 
 final networkUtils = _NetworkUtils();
-bool _isLoggedIn = true;
 
 class _NetworkUtils {
   Future<void> checkNetOnClick() async {
+    bool isNewUser = await sf.isNewUser();
     bool result = await InternetConnectionChecker().hasConnection;
     if (result == false) {
       showSnackBar(msg: "Please check your internet");
       return;
-    } else if (_isLoggedIn == true) {
+    } else if (isNewUser == false) {
       appRouter.push(const HomeScreen());
-    } else if (_isLoggedIn == false) {
-      // appRouter.push(const OnBoardingScreen());
+      return;
+    } else if (isNewUser == true) {
+      appRouter.push(const OnBoardingScreen());
+      return;
     } else {
-      // appRouter.push(const OnBoardingScreen());
+      appRouter.push(const OnBoardingScreen());
+      return;
     }
   }
 
